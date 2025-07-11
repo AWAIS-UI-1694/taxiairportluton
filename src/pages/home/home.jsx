2b7baa080ed3ac678403dc9302172e0b77964bd7
AWAIS-UI-1694 <awaisatoffice@gmail.com>
2025-07-11 21:02:03 +0500

    meta

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -12,12 +12,20 @@ import StationsSection from "./homeComponents/stationsSectiono";
 import { FaUserFriends } from "react-icons/fa";
 import Cab from "./homeComponents/cab";
 import Download from "../../utils/download";
+import { Helmet } from "react-helmet-async";
 
 const Home = () => {
 
 
 
   return (
+    <>
+    <Helmet>
+        <title>Taxi Airport Luton—Airport Taxi Luton—Luton Airport Transfer</title>
+        <meta name="description" content="Taxi Airport Luton offers 24/7 taxi services to and from Luton Airport, offers a meet-and-greet service at arrivals, and offers flight monitoring." />
+        <meta name="keywords" content="Taxi Airport Luton, Luton Taxi, Luton Cab, Luton Minicab, Luton Airport Transfers, Luton Airport Taxi, Luton Taxi Numbers, Luton Station Taxi, Taxi Near Me in Luton, Luton Airport Taxi Service, Airport Taxi Luton, Luton Taxi Firms, Book Taxi To Luton Airport" />
+        <link rel="canonical" href="https://www.taxiairportluton.com/about" />
+      </Helmet>
     <div className="">
       {/* Hero Section */}
       <Hero />
@@ -35,6 +43,7 @@ const Home = () => {
     {/* download */}
     <Download />
     </div>
+    </>
   );
 };
 
