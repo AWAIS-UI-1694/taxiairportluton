49459c8c117840460e4d65bf3e1e6489e6077131
AWAIS-UI-1694 <awaisatoffice@gmail.com>
2025-07-11 21:12:04 +0500

    e

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { HelmetProvider } from 'react-helmet-async'; 
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />);
+
+root.render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
+);
